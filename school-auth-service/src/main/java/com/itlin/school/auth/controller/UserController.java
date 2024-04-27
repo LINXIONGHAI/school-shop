c042d0c53f571a07ac49f40ad9246fa3e74d4dbd
“linxionghai” <1246113457@qq.com>
2024-04-27 22:34:40 +0800

    根据用户id查询用户信息

--- school-auth-service/src/main/java/com/itlin/school/auth/controller/UserController.java
+++ school-auth-service/src/main/java/com/itlin/school/auth/controller/UserController.java
@@ -1,6 +1,8 @@
 package com.itlin.school.auth.controller;
 
 import com.itlin.school.auth.service.UserService;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -8,14 +10,16 @@ import javax.annotation.Resource;
 
 @RestController
 @RequestMapping("/api/user/v1")
+@Slf4j
 public class UserController {
 
 
     @Resource
     private UserService userService;
 
-    @RequestMapping("get")
-    public Object get(){
-        return userService.queryById(null);
+    @RequestMapping("get/{user_id}")
+    public Object get(@PathVariable Long user_id){
+        log.info("UserController:get:user_id={}",user_id);
+        return userService.queryById(user_id);
     }
 }
