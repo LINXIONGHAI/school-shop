d45a34d1550f5b8c59d09e0bfc1fba72bfd3ddbf
“linxionghai” <1246113457@qq.com>
2024-04-29 16:29:22 +0800

    添加购物车

--- school-product-service/src/main/java/com/itlin/product/service/impl/CartServiceImpl.java
+++ school-product-service/src/main/java/com/itlin/product/service/impl/CartServiceImpl.java
@@ -21,11 +21,6 @@ import org.springframework.stereotype.Service;
 
 import javax.annotation.Resource;
 import java.math.BigDecimal;
-import java.util.ArrayList;
-import java.util.HashMap;
-import java.util.List;
-import java.util.Map;
-import java.util.concurrent.TimeUnit;
 
 @Service
 public class CartServiceImpl implements CartService {
